9b39ca831e8d69d3b592baf5d791b945accc7efa
Andrew Hill <andrew@portworx.com>
2020-01-22 15:28:37 +0000

    wait for cluster delete

--- destroy.sh
+++ destroy.sh
@@ -4,4 +4,4 @@ vpc=$(eksctl utils describe-stacks --region $REGION --cluster $EKS_CLUSTER_NAME 
 profile=$(aws ec2 describe-instances --filters "Name=vpc-id,Values=$vpc" --region $REGION --query Reservations[0].Instances[0].IamInstanceProfile.Arn --output text | cut -f 2 -d /)
 role=$(aws iam get-instance-profile --instance-profile-name $profile --region $REGION --query InstanceProfile.Roles[0].RoleName --output text)
 aws iam delete-role-policy --role-name $role --policy-name px-eks-policy --region $REGION
-eksctl delete cluster --region $REGION --name $EKS_CLUSTER_NAME
+eksctl delete cluster --region $REGION --name $EKS_CLUSTER_NAME --wait
